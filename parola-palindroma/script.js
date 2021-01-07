cdcc5c38e6cc02864cf5cc5791a8b41aa6f605d0
Michele Cofano <brutalmachinelol@gmail.com>
2021-01-07 15:49:23 +0100

    Inserimento prompt per richiesta parola

---
+++ parola-palindroma/script.js
@@ -0,0 +1,7 @@
+// Svolgimento -->
+
+//Chiedere all'utente una parola_utente
+var parolaUtente = prompt('Inserisci una parola');
+console.log(parolaUtente);
+
+// verificare, tramite delle funzioni, se la parola inserita dall'utente è palindroma o meno
